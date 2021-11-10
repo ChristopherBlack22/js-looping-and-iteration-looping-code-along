64b8071d0fcf06a171701288879fd115fbeeb929
Christopher <christopher.black.2209@googlemail.com>
2021-11-10 01:11:27 +0000

    commit tree f83ce25dbc9204df6e013e4309a4960b4e21b444

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const cards = [];
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+function countDown(integer) {
+    let i = integer;
+    while (i > -1 ) {
+        console.log(i);
+        i--
+    }
+}
